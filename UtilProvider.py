67f624c8df985768a34f78a7dc35640a3b16001f
Aditya Ganti <adi10rox@gmail.com>
2024-02-01 12:35:02 -0800

    add indent count and fix smells

--- UtilProvider.py
+++ UtilProvider.py
@@ -1,74 +1,80 @@
 import json
 import re
 
+
 class UtilProvider:
-    def count_lines(self, code_blob: str) -> int:
+    @staticmethod
+    def count_lines(code_blob: str) -> int:
         # Count the number of lines in a string
         return len(code_blob.splitlines())
-    
-    def count_words(self, code_blob: str) -> int:
+
+    @staticmethod
+    def count_words(code_blob: str) -> int:
         # Count the number of words in a string
         return len(code_blob.split())
-    
-    def unmarshall_json_object(self, json_object: str) -> dict:
+
+    @staticmethod
+    def unmarshall_json_object(json_object: str) -> dict:
         # Unmarshall a JSON object
         return json.loads(json_object)
-    
-    def marshall_json_object(self, codestring: str) -> str:
+
+    @staticmethod
+    def marshall_json_object(codestring: str) -> str:
         # Marshall a JSON object
         return json.dumps(codestring)
-    
-    def jaccard_similarity(self, set1: set, set2: set) -> float:
+
+    @staticmethod
+    def jaccard_similarity(set1: set, set2: set) -> float:
         # Calculate the Jaccard index of two sets
         return len(set1.intersection(set2)) / len(set1.union(set2))
-    
-    def parameter_list_cleanup(self, parameter_list: list) -> list:
+
+    @staticmethod
+    def parameter_list_cleanup(parameter_list: list) -> list:
         # Clean up the parameter list by stripping and removing type definitions
         return [parameter.strip().split(':')[0] for parameter in parameter_list]
-    
-    def remove_empty_strings_from_list(self, string_list: list) -> list:
+
+    @staticmethod
+    def remove_empty_strings_from_list(string_list: list) -> list:
         # Remove empty strings from a list of strings
         return list(filter(None, string_list))
-    
-    def is_empty_string(self, string: str) -> bool:
+
+    @staticmethod
+    def is_empty_string(string: str) -> bool:
         # Check if a string has no whitespace characters
         return not string.strip()
-    
-    def is_import_line(self, line: str) -> bool:
+
+    @staticmethod
+    def is_import_line(line: str) -> bool:
         # Check if a line is an import line
         return line.startswith("import") or line.startswith("from")
 
-    def is_global_const_line(self, line: str) -> bool:
+    @staticmethod
+    def is_global_const_line(line: str) -> bool:
         # Check if a line is a global constant line
         pattern = re.compile('^[A-Z_]+')
         return True if pattern.match(line) else False
-    
+
     # Function should be able to preserve the preceding whitespaces
-    def is_function_definition_line(self, line: str) -> bool:
+    @staticmethod
+    def is_function_definition_line(line: str) -> bool:
         # Check if a line is a function definition line
-        pattern  = re.compile('\s*def')
+        pattern = re.compile(r'\s*def')
         return True if pattern.match(line) else False
-        #return line.startswith("def")
-    
-    def is_class_definition_line(self, line: str) -> bool:
+
+    @staticmethod
+    def is_class_definition_line(line: str) -> bool:
         # Check if a line is a class definition line
         return line.startswith("class")
-    
-    def is_module_or_script_line(self, line: str) -> bool:
+
+    @staticmethod
+    def is_module_or_script_line(line: str) -> bool:
         # Check if a line is a module or script line
         pattern = re.compile('^if __[a-zA-Z]+__ ==')
         if pattern.match(line):
             return True
         return False
-    
 
-
-"""
-Cannot change the execution of the function (preserve functionality)
-post refactoring, it has to have - 
-
-changed internal structure without changing the external behavior
-
-same i/p and o/p
-
-"""
+    @staticmethod
+    def indent_count(line: str) -> int:
+        # Count the number of leading spaces in a line
+        return len(line) - len(line.lstrip())
