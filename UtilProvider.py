4d9e063fb4411b9a49091390560b221cc4d4c2a7
Aditya Ganti <adi10rox@gmail.com>
2024-01-31 17:04:27 -0800

    Update UtilProvider.py

--- UtilProvider.py
+++ UtilProvider.py
@@ -18,7 +18,7 @@ class UtilProvider:
         # Marshall a JSON object
         return json.dumps(codestring)
     
-    def jaccard_index(self, set1: set, set2: set) -> float:
+    def jaccard_similarity(self, set1: set, set2: set) -> float:
         # Calculate the Jaccard index of two sets
         return len(set1.intersection(set2)) / len(set1.union(set2))
     
@@ -37,10 +37,18 @@ class UtilProvider:
     def is_import_line(self, line: str) -> bool:
         # Check if a line is an import line
         return line.startswith("import") or line.startswith("from")
+
+    def is_global_const_line(self, line: str) -> bool:
+        # Check if a line is a global constant line
+        pattern = re.compile('^[A-Z_]+')
+        return True if pattern.match(line) else False
     
+    # Function should be able to preserve the preceding whitespaces
     def is_function_definition_line(self, line: str) -> bool:
         # Check if a line is a function definition line
-        return line.startswith("def")
+        pattern  = re.compile('\s*def')
+        return True if pattern.match(line) else False
+        #return line.startswith("def")
     
     def is_class_definition_line(self, line: str) -> bool:
         # Check if a line is a class definition line
@@ -49,7 +57,18 @@ class UtilProvider:
     def is_module_or_script_line(self, line: str) -> bool:
         # Check if a line is a module or script line
         pattern = re.compile('^if __[a-zA-Z]+__ ==')
-        print(pattern.match(line))
         if pattern.match(line):
             return True
         return False
+    
+
+
+"""
+Cannot change the execution of the function (preserve functionality)
+post refactoring, it has to have - 
+
+changed internal structure without changing the external behavior
+
+same i/p and o/p
+
+"""
